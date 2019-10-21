e9d2ba64b1bdd767eaf4a339106852af6cb7bed0
Anders Christian Sørensen <asore@tdc.dk>
2019-10-21 11:42:21 +0200

    Add builder for VirtualBox

---
+++ packer/win2019/sysprep.bat
@@ -0,0 +1,5 @@
+mkdir %WINDIR%\Setup\Scripts
+echo wmic useraccount where "name='vagrant'" set passwordexpires=false > %WINDIR%\Setup\Scripts\SetupComplete.cmd
+echo wmic useraccount where "name='Administrator'" set passwordexpires=false >> %WINDIR%\Setup\Scripts\SetupComplete.cmd
+
+%WINDIR%\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:A:\Autounattend.xml
