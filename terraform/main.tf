19e77ac389804464846cb8942e063e07d07cf146
Anders Christian Sørensen <asore@tdc.dk>
2019-10-21 11:42:22 +0200

    Use terraform-aws-{nomad,consul} to create cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,101 +1,144 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "aws" {
-    version = "~> 2.0"
-    region  = "eu-north-1"
-    profile = "nuuday_digital_dev"
+  region = "eu-north-1"
+  profile = "nuuday_digital_dev"
 }
 
-data "aws_availability_zones" "available" {
-    state = "available"
+data "aws_ami" "nomad_consul" {
+  most_recent = true
+
+  owners = ["self"]
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "name"
+    values = ["nomad-consul-ubuntu18-*"]
+  }
 }
 
-data "aws_ami" "windows" {
-    most_recent      = true
-    owners           = ["amazon"]
+data "template_file" "user_data_server" {
+  template = file("${path.module}/scripts/user-data-server.sh")
 
-    filter {
-        name = "name"
-        values = ["Windows_Server-2019-English-Core-ContainersLatest-*"]
+  vars = {
+    cluster_tag_key = var.cluster_tag_key
+    cluster_tag_value = var.cluster_tag_value
+    num_servers = var.num_servers
+  }
+}
+
+# Gives back the current region we're deploying into.
+# We'll use this to get "availability zones" in the next step.
+data "aws_region" "current" {}
+
+# Gives back available "availability zones" (AZ) in the current region.
+# We'll use that to establish networks in each AZ to increase resiliency of our cluster.
+data "aws_availability_zones" "all" {
+  state = "available"
+}
+
+# NETWORK
+
+resource "aws_vpc" "nomad" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_" 
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${aws_vpc.nomad.id}"
+}
+
+module "servers" {
+  source = "github.com/hashicorp/terraform-aws-consul//modules/consul-cluster?ref=v0.7.0"
+
+  cluster_name = "${var.cluster_name}-server"
+  cluster_size = var.num_servers
+  instance_type = var.server_instance_type
+
+  # EC2 instances will be tagged with these tags in AWS.
+  # They're used by Consul for auto-discovering each other and forming a cluster.
+  cluster_tag_key = var.cluster_tag_key
+  cluster_tag_value = var.cluster_tag_value
+
+  ami_id = var.ami_id == null ? data.aws_ami.nomad_consul.image_id : var.ami_id
+  user_data = data.template_file.user_data_server.rendered
+
+  vpc_id = "${aws_vpc.nomad.id}"
+  subnet_ids = data.aws_subnet_ids.all.ids
+
+  allowed_ssh_cidr_blocks = [
+    "128.76.39.70/32"
+  ]
+
+  allowed_inbound_cidr_blocks = [
+    "128.76.39.70/32"
+  ]
+
+  ssh_key_name = var.ssh_key_name
+
+  tags = [
+    {
+      key = "Team"
+      value = "odin-platform"
+      propagate_at_launch = true
     }
+  ]
 }
 
-resource "aws_vpc" "packer" {
-    cidr_block = "10.1.0.0/16"
+data "template_file" "user_data_client" {
+  template = file("${path.module}/scripts/user-data-client.sh")
 
-    tags = {
-        Name = "packer"
+  vars = {
+    cluster_tag_key = var.cluster_tag_key
+    cluster_tag_value = var.cluster_tag_value
+  }
+}
+
+module "clients" {
+  source = "github.com/hashicorp/terraform-aws-nomad//modules/nomad-cluster?ref=v0.5.0"
+
+  cluster_name = "${var.cluster_name}-client"
+  instance_type = var.instance_type
+
+  # EC2 instances will be tagged with these tags in AWS.
+  # They're used by Consul for auto-discovering each other and forming a cluster.
+  cluster_tag_key = "nomad-clients"
+  cluster_tag_value = var.cluster_name
+
+  # Fixed size cluster for now.
+  min_size = var.num_clients
+  max_size = var.num_clients
+  desired_capacity = var.num_clients
+
+  ami_id = var.ami_id == null ? data.aws_ami.nomad_consul.image_id : var.ami_id
+  user_data = data.template_file.user_data_client.rendered
+
+  vpc_id = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnet_ids.default.ids
+
+  allowed_ssh_cidr_blocks = [
+    "128.76.39.70/32"
+  ]
+
+  allowed_inbound_cidr_blocks = [
+    "128.76.39.70/32"
+  ]
+
+  ssh_key_name = var.ssh_key_name
+
+  tags = [
+    {
+      key = "Team"
+      value = "odin-platform"
+      propagate_at_launch = true
     }
-}
-
-resource "aws_subnet" "packer" {
-    vpc_id     = "${aws_vpc.packer.id}"
-    cidr_block = "10.1.0.0/24"
-
-    tags = {
-        Name = "packer"
-    }
-}
-
-resource "aws_vpc" "default" {
-    cidr_block = "10.0.0.0/16"
-
-    tags = {
-        Name = "sitecore-9-test"
-    }
-}
-
-resource "aws_subnet" "cd_az_1" {
-    vpc_id     = "${aws_vpc.default.id}"
-    cidr_block = "10.0.1.0/24"
-
-    availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-    tags = {
-        Name = "cd-az-1"
-    }
-}
-
-resource "aws_subnet" "cd_az_2" {
-    vpc_id     = "${aws_vpc.default.id}"
-    cidr_block = "10.0.2.0/24"
-
-    availability_zone = "${data.aws_availability_zones.available.names[1]}"
-
-    tags = {
-        Name = "cd-az-1"
-    }
-}
-
-resource "aws_placement_group" "spread" {
-    name = "spread"
-    strategy = "spread"
-}
-
-
-resource "aws_launch_configuration" "cd_config" {
-    name_prefix = "cd-"
-    image_id = "${data.aws_ami.windows.id}"
-    instance_type = "t3.small"
-
-    lifecycle {
-        create_before_destroy = true
-    }
-}
-
-resource "aws_autoscaling_group" "cd" {
-    name = "cd-auto-scaling-group"
-    launch_configuration = "${aws_launch_configuration.cd_config.name}"
-    placement_group = "${aws_placement_group.spread.id}"
-
-    vpc_zone_identifier = [
-        "${aws_subnet.cd_az_1.id}",
-        "${aws_subnet.cd_az_2.id}",
-    ]
-
-    min_size = 1
-    max_size = 2
-
-    lifecycle {
-        create_before_destroy = true
-    }
+  ]
 }
 
---
+++ terraform/variables.tf
@@ -0,0 +1,69 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+# AWS_DEFAULT_REGION
+
+variable "ami_id" {
+  description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/nomad-consul-ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
+  type        = string
+  default     = null
+}
+
+variable "cluster_name" {
+  description = "What to name the cluster and all of its associated resources"
+  type        = string
+  default     = "nomad-example"
+}
+
+variable "instance_type" {
+  description = "What kind of instance type to use for the nomad clients"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "server_instance_type" {
+  description = "What kind of instance type to use for the nomad servers"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "num_servers" {
+  description = "The number of server nodes to deploy. We strongly recommend using 3 or 5."
+  type        = number
+  default     = 3
+}
+
+variable "num_clients" {
+  description = "The number of client nodes to deploy. You can deploy as many as you need to run your jobs."
+  type        = number
+  default     = 2
+}
+
+variable "cluster_tag_key" {
+  description = "The tag the EC2 Instances will look for to automatically discover each other and form a cluster."
+  type        = string
+  default     = "nomad-servers"
+}
+
+variable "cluster_tag_value" {
+  description = "Add a tag with key var.cluster_tag_key and this value to each Instance in the ASG. This can be used to automatically find other Consul nodes and form a cluster."
+  type        = string
+  default     = "auto-join"
+}
+
+variable "ssh_key_name" {
+  description = "The name of an EC2 Key Pair that can be used to SSH to the EC2 Instances in this cluster. Set to an empty string to not associate a Key Pair."
+  type        = string
+  default     = "ThinkCentre"
+}
+
+variable "vpc_cidr_block" {
+  description = "The IP range in CIDR notation which will constitute the VPC in which Nomad/Consul VMs will be deployed."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
