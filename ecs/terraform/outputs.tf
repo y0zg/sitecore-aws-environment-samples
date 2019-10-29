e5038d8d4da8120280e33b6742b5a6668c750b3f
Anders Christian Sørensen <asore@tdc.dk>
2019-10-29 09:23:48 +0100

    Output load balancer's FQDN

--- ecs/terraform/outputs.tf
+++ ecs/terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "user_data_ecs_windows" {
-  value = "${data.template_file.user_data_windows.rendered}"
+  value = data.template_file.user_data_windows.rendered
+}
+
+output "alb_fqdn" {
+  value = aws_lb.lb.dns_name
 }
