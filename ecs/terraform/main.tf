d55034b8178c629284317374a1e0aae555a1c095
Anders Christian Sørensen <asore@tdc.dk>
2019-10-30 20:03:33 +0100

    Move from eu-north-1 to eu-central-1 for RDS SQL Server AlwaysOn capability

--- ecs/terraform/main.tf
+++ ecs/terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-north-1"
+  region  = "eu-central-1"
   profile = "nuuday_digital_dev"
 }
 
@@ -19,7 +19,7 @@ module "vpc" {
 
   cidr = "10.1.0.0/16"
 
-  azs              = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  azs              = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets   = ["10.1.11.0/24", "10.1.12.0/24", "10.1.13.0/24"]
   database_subnets = ["10.1.20.0/24", "10.1.21.0/24", "10.1.22.0/24"]
