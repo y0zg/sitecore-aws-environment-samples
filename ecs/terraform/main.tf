62db61205c7f9665ac80d3e317294c76e1be45b8
Anders Christian Sørensen <asore@tdc.dk>
2019-10-30 20:05:31 +0100

    ECS Service for Sitecore CD and explicit external load balancer

--- ecs/terraform/main.tf
+++ ecs/terraform/main.tf
@@ -49,12 +49,6 @@ data "aws_ami" "windows_ecs" {
   }
 }
 
-#module "ecs_instance_profile" {
-#  source = "github.com/terraform-aws-modules/terraform-aws-ecs//modules/ecs-instance-profile?ref=v2.0.0"
-#
-#  name = local.cluster_name
-#}
-
 resource "aws_iam_policy" "instance_role_cloudwatch" {
   name        = "ECS-CloudWatchLogs"
   path        = "/ecs/"
@@ -123,10 +117,12 @@ module "cluster" {
 }
 
 resource "aws_ecs_service" "iis_sample" {
-  name            = "iis_sample"
-  cluster         = module.cluster.this_ecs_cluster_id
-  launch_type     = "EC2"
-  task_definition = aws_ecs_task_definition.iis.arn
+  name                               = "iis_sample"
+  cluster                            = module.cluster.this_ecs_cluster_id
+  launch_type                        = "EC2"
+  task_definition                    = aws_ecs_task_definition.iis.arn
+  deployment_minimum_healthy_percent = 100
+  health_check_grace_period_seconds  = 120
 
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs_instances.id
@@ -139,8 +135,52 @@ resource "aws_ecs_service" "iis_sample" {
     field = "attribute:ecs.availability-zone"
   }
 
-  desired_count = 3
+  desired_count = 1
 }
+
+# CD
+resource "aws_ecs_task_definition" "cd" {
+  family = "cd"
+
+  container_definitions = <<EOF
+[
+	{
+		"name": "cd",
+    "image": "anarsdk/sitecore-cd:9.2.0-attempt1",
+    "cpu": 512,
+    "memory": 2048,
+		"portMappings": [
+			{
+				"containerPort": 80
+			}
+		]
+	}
+]
+EOF
+}
+
+resource "aws_ecs_service" "cd" {
+  name                               = "cd"
+  cluster                            = module.cluster.this_ecs_cluster_id
+  launch_type                        = "EC2"
+  task_definition                    = aws_ecs_task_definition.cd.arn
+  deployment_minimum_healthy_percent = 100
+  health_check_grace_period_seconds  = 120
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.cd.id
+    container_name   = "cd"
+    container_port   = 80
+  }
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  desired_count = 1
+}
+
 
 data "template_file" "user_data_windows" {
   template = file("${path.module}/templates/ec2-user-data-ecs-windows.script")
@@ -155,24 +195,18 @@ resource "aws_security_group" "allow_all_internal" {
 
   ingress {
     from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
+    to_port   = 0
+    protocol  = "-1"
 
-    cidr_blocks = ["128.76.39.70/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
 
-resource "aws_security_group" "allow_rdp" {
-  name   = "allow_rdp_internal"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port = 3389
-    to_port   = 3389
-    protocol  = "tcp"
-
-    cidr_blocks = ["128.76.39.70/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -185,7 +219,7 @@ resource "aws_security_group" "allow_lb_ingress" {
     to_port   = 65535
     protocol  = "tcp"
 
-    security_groups = [aws_security_group.lb.id]
+    security_groups = [aws_security_group.lb_external.id]
   }
 }
 
@@ -197,17 +231,16 @@ module "ecs_instances" {
   instance_type = "t3.large"
   security_groups = [
     module.vpc.default_security_group_id,
-    aws_security_group.allow_rdp.id,
     aws_security_group.allow_lb_ingress.id,
     aws_security_group.allow_all_internal.id
   ]
 
   asg_name             = "${local.cluster_name}-asg"
-  vpc_zone_identifier  = module.vpc.private_subnets
+  vpc_zone_identifier  = module.vpc.public_subnets
   health_check_type    = "EC2"
-  min_size             = 3
-  max_size             = 6
-  desired_capacity     = 3
+  min_size             = 2
+  max_size             = 2
+  desired_capacity     = 2
   iam_instance_profile = data.aws_iam_role.ecs_instance.id
   key_name             = "ASORE"
 
@@ -224,13 +257,20 @@ module "ecs_instances" {
 
 # Ingress
 
-resource "aws_security_group" "lb" {
-  name   = "${local.cluster_name}-lb-sg"
+resource "aws_security_group" "lb_external" {
+  name   = "${local.cluster_name}-lb-external-sg"
   vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -250,18 +290,39 @@ resource "aws_lb_target_group" "ecs_instances" {
   vpc_id   = module.vpc.vpc_id
 }
 
-resource "aws_lb" "lb" {
+resource "aws_lb_target_group" "cd" {
+  name     = "${local.cluster_name}-cd-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener_rule" "cd" {
+  listener_arn = aws_lb_listener.frontend.arn
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.cd.arn
+  }
+
+  condition {
+    field = "host-header"
+    values = [aws_route53_record.cd.fqdn]
+  }
+}
+
+resource "aws_lb" "lb_external" {
   name            = "${local.cluster_name}-lb"
   subnets         = module.vpc.public_subnets
-  security_groups = [aws_security_group.lb.id]
+  security_groups = [aws_security_group.lb_external.id]
 }
 
 resource "aws_lb_listener" "frontend" {
-  load_balancer_arn = aws_lb.lb.id
+  load_balancer_arn = aws_lb.lb_external.id
   port              = 443
   protocol          = "HTTPS"
 
-  certificate_arn = aws_acm_certificate_validation.cd.certificate_arn
+  certificate_arn = aws_acm_certificate_validation.iis_sample.certificate_arn
 
   default_action {
     target_group_arn = aws_lb_target_group.ecs_instances.id
@@ -269,9 +330,23 @@ resource "aws_lb_listener" "frontend" {
   }
 }
 
+resource "aws_lb_listener_certificate" "cd" {
+  listener_arn = aws_lb_listener.frontend.arn
+  certificate_arn = aws_acm_certificate_validation.cd.certificate_arn
+}
+
 data "aws_route53_zone" "nuuday" {
   name         = "aws.nuuday.nu."
   private_zone = false
+}
+
+resource "aws_route53_record" "iis_sample" {
+  zone_id = data.aws_route53_zone.nuuday.id
+  name    = "iis-sample-dev.${data.aws_route53_zone.nuuday.name}"
+  type    = "CNAME"
+  ttl     = "300"
+
+  records = [aws_lb.lb_external.dns_name]
 }
 
 resource "aws_route53_record" "cd" {
@@ -280,10 +355,40 @@ resource "aws_route53_record" "cd" {
   type    = "CNAME"
   ttl     = "300"
 
-  records = [aws_lb.lb.dns_name]
+  records = [aws_lb.lb_external.dns_name]
 }
 
-resource "aws_route53_record" "cert_validation" {
+
+# Certificate: IIS
+resource "aws_route53_record" "iis_cert_validation" {
+  zone_id = data.aws_route53_zone.nuuday.id
+  name    = aws_acm_certificate.iis_sample.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.iis_sample.domain_validation_options.0.resource_record_type
+  ttl     = "60"
+
+  records = [aws_acm_certificate.iis_sample.domain_validation_options.0.resource_record_value]
+}
+
+resource "aws_acm_certificate" "iis_sample" {
+  domain_name       = aws_route53_record.iis_sample.fqdn
+  validation_method = "DNS"
+
+  tags = {
+    Team = "odin-platform"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "iis_sample" {
+  certificate_arn         = aws_acm_certificate.iis_sample.arn
+  validation_record_fqdns = [aws_route53_record.iis_cert_validation.fqdn]
+}
+
+# Certificate: CD
+resource "aws_route53_record" "cd_cert_validation" {
   zone_id = data.aws_route53_zone.nuuday.id
   name    = aws_acm_certificate.cd.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.cd.domain_validation_options.0.resource_record_type
@@ -291,6 +396,7 @@ resource "aws_route53_record" "cert_validation" {
 
   records = [aws_acm_certificate.cd.domain_validation_options.0.resource_record_value]
 }
+
 
 resource "aws_acm_certificate" "cd" {
   domain_name       = aws_route53_record.cd.fqdn
@@ -307,7 +413,7 @@ resource "aws_acm_certificate" "cd" {
 
 resource "aws_acm_certificate_validation" "cd" {
   certificate_arn         = aws_acm_certificate.cd.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [aws_route53_record.cd_cert_validation.fqdn]
 }
 
 # Database
@@ -330,8 +436,12 @@ module "rds" {
   password = "YourPwdShouldBeLongAndSecure!"
   port     = "1433"
 
+  multi_az = false
+
   subnet_ids             = module.vpc.database_subnets
-  vpc_security_group_ids = [aws_security_group.allow_all_internal.id]
+  vpc_security_group_ids = [
+    aws_security_group.allow_all_internal.id,
+  ]
   publicly_accessible    = true
   timezone               = "Central Standard Time"
   family                 = "sqlserver-web-14.0"
@@ -340,3 +450,4 @@ module "rds" {
     Team = "odin-platform"
   }
 }
+
--- ecs/terraform/outputs.tf
+++ ecs/terraform/outputs.tf
@@ -2,8 +2,8 @@ output "user_data_ecs_windows" {
   value = data.template_file.user_data_windows.rendered
 }
 
-output "alb_fqdn" {
-  value = aws_lb.lb.dns_name
+output "lb_external_fqdn" {
+  value = aws_lb.lb_external.dns_name
 }
 
 output "cd_fqdn" {
