f107ff35b62e7dbd78a765fbf281182e9e18cc51
Anders Christian Sørensen <asore@tdc.dk>
2019-11-04 09:27:33 +0100

    Perform HTTPS redirect in ALB

--- ecs/terraform/main.tf
+++ ecs/terraform/main.tf
@@ -235,6 +235,22 @@ resource "aws_lb" "lb_external" {
   security_groups = [aws_security_group.lb_external.id]
 }
 
+resource "aws_lb_listener" "frontend_http" {
+  load_balancer_arn = aws_lb.lb_external.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener" "frontend" {
   load_balancer_arn = aws_lb.lb_external.id
   port              = 443
