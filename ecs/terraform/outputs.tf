ef0830fa6a987f3100ab01e291fc77c103f76287
Anders Christian Sørensen <asore@tdc.dk>
2019-11-04 10:38:44 +0100

    Output RDS password and NAT Gateway IPs

--- ecs/terraform/outputs.tf
+++ ecs/terraform/outputs.tf
@@ -10,3 +10,12 @@ output "rds_endpoint" {
   value = module.rds.this_db_instance_endpoint
 }
 
+output "rds_password" {
+  value = random_string.db_password.result
+}
+
+output "nat_gateway_public_ips" {
+  description = "Public IPs of the provisioned NAT gateways"
+  value = module.vpc.nat_public_ips
+}
+
