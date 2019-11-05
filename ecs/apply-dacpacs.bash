fb7eb9a2e1d0b26da5c398a2e0544e567c829989
Anders Christian Sørensen <asore@tdc.dk>
2019-11-05 12:58:31 +0100

    Download sqlpackage(1) from apply-dacpacs.sh

--- ecs/apply-dacpacs.bash
+++ ecs/apply-dacpacs.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+SQLPACKAGE_URL="https://download.microsoft.com/download/7/5/d/75d3ba2d-2f6b-46e7-a0ef-3eaba605e935/sqlpackage-linux-x64-en-US-15.0.4573.2.zip"
 SQLPACKAGE="$(pwd)/sqlpackage/sqlpackage"
 DACPAC_FILES="$(pwd)/dacpac/*.dacpac"
+
+curl -o sqlpackage.zip $SQLPACKAGE_URL
+unzip sqlpackage.zip -d $(pwd)/sqlpackage
+chmod +x $SQLPACKAGE
 
 for filepath in $DACPAC_FILES; do
 
