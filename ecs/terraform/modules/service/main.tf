143ca9a961904f37f2b29e7000982477c02dfa83
Anders Christian Sørensen <asore@tdc.dk>
2019-11-07 16:33:10 +0100

    Explicitly configure and relax health checks in target groups

--- ecs/terraform/modules/service/main.tf
+++ ecs/terraform/modules/service/main.tf
@@ -12,15 +12,27 @@ data "aws_lb_listener" "this" {
 }
 
 resource "aws_ecs_task_definition" "this" {
-  family = var.name
+  family                = var.name
   container_definitions = var.container_definitions_json
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "${var.name}-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name       = "${var.name}-tg"
+  port       = 8080
+  protocol   = "HTTP"
+  slow_start = 60
+  vpc_id     = var.vpc_id
+
+  health_check {
+    enabled             = true
+    port                = "traffic-port"
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    matcher             = "200-499"
+  }
 }
 
 resource "aws_ecs_service" "this" {
