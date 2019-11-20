2cad1f98d008c2f8d2ac6d0c921001257cee292b
Anders Christian Sørensen <asore@tdc.dk>
2019-11-20 15:48:15 +0100

    Don't output service FQDNs

--- ecs/terraform/outputs.tf
+++ ecs/terraform/outputs.tf
@@ -2,10 +2,6 @@ output "lb_external_fqdn" {
   value = aws_lb.lb_external.dns_name
 }
 
-output "service_fqdns" {
-  value = module.service.*.service_fqdn
-}
-
 output "rds_host" {
   value = module.rds.this_db_instance_address
 }
