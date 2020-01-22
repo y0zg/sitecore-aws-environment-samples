4918dfc356d2d17a79af5e4c94b8dea4ff2d8a4a
Anders Christian Sørensen <asore@tdc.dk>
2020-01-22 09:27:05 +0100

    Add ephemeral environment dependencies

---
+++ ecs/terraform/dev.tf
@@ -0,0 +1,40 @@
+# Ephemeral Dev Environments
+
+locals {
+  zone = "aws.nuuday.nu."
+  domain_name = "scdemo.aws.nuuday.nu"
+}
+
+data "aws_route53_zone" "this" {
+  name = local.zone
+}
+
+resource "aws_route53_record" "dev" {
+  name = "*.${local.domain_name}"
+  type = "CNAME"
+  ttl  = "300"
+
+  zone_id = data.aws_route53_zone.this.id
+
+  records = [aws_lb.lb_external.dns_name]
+}
+
+module "dev_certificate" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> v2.0"
+
+  domain_name  = local.domain_name
+  zone_id      = data.aws_route53_zone.this.id
+
+  subject_alternative_names = [ 
+    "*.${local.domain_name}",
+  ]
+
+  tags = local.common_tags
+}
+
+resource "aws_lb_listener_certificate" "dev_wildcard_certificate" {
+  listener_arn    = aws_lb_listener.frontend.arn
+  certificate_arn = module.dev_certificate.this_acm_certificate_arn
+}
+
