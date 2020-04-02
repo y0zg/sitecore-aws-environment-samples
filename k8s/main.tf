9aeac7cb904b6c259fb0748d9a02c9f8e1a5a3b5
Anders Christian Sørensen <asore@nuuday.dk>
2020-04-02 14:11:56 +0200

    k8s: Provision NLB using Terraform and reference it from nginx-ingress-controller

--- k8s/main.tf
+++ k8s/main.tf
@@ -26,6 +26,10 @@ data "aws_region" "current" {}
 locals {
   cluster_name = "test-eks-${random_string.suffix.result}"
 
+  ingress_tags = {
+    "kubernetes.io/service-name" = "default/nginx-ingress-controller"
+    "kubernetes.io/cluster/${local.cluster_name}" = "owned"
+  }
 
   tags = {
     team       = "odin-platform"
@@ -70,6 +74,98 @@ module "vpc" {
   }
 }
 
+resource "aws_lb" "external" {
+  name               = "${local.cluster_name}-ext"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = module.vpc.public_subnets
+
+  tags = merge(
+    local.tags,
+    local.ingress_tags,
+  )
+}
+
+resource "aws_lb_target_group" "http" {
+  name_prefix = "http"
+  port        = 32080
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = merge(
+    local.tags,
+    local.ingress_tags,
+  )
+}
+
+resource "aws_lb_target_group" "https" {
+  name_prefix = "https"
+  port        = 32443
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = merge(
+    local.tags,
+    local.ingress_tags,
+  )
+}
+
+resource "aws_lb_listener" "external_http" {
+  load_balancer_arn = aws_lb.external.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_listener" "external_https" {
+  load_balancer_arn = aws_lb.external.arn
+  port              = 443
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https.arn
+  }
+}
+
+resource "aws_security_group" "worker_http_ingress" {
+  name_prefix = "${local.cluster_name}-ingress-http-"
+  description = "Allows HTTP access from anywhere to the ingress NodePorts"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP from NLB"
+    from_port   = 32080
+    to_port     = 32080
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_https_ingress" {
+  name_prefix = "${local.cluster_name}-ingress-https-"
+  description = "Allows HTTPS access from anywhere to the ingress NodePorts"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP from NLB"
+    from_port   = 32443
+    to_port     = 32443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
+
 module "eks" {
   source = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v10.0.0"
 
@@ -78,17 +174,24 @@ module "eks" {
   enable_irsa  = true
   vpc_id       = module.vpc.vpc_id
 
-  node_groups = [
-    {
-      name             = "primary"
-      instance_type    = "t3.nano"
-      max_size         = var.linux_workers_count
-      min_size         = var.linux_workers_count
-      desired_capacity = var.linux_workers_count
-    },
-  ]
-
   worker_groups = [
+    {
+      instance_type    = "t3.large"
+      platform         = "linux"
+      asg_max_size         = var.linux_workers_count
+      asg_min_size         = var.linux_workers_count
+      asg_desired_capacity = var.linux_workers_count
+      target_group_arns    = [
+        aws_lb_target_group.http.id,
+        aws_lb_target_group.https.id,
+      ]
+
+      additional_security_group_ids = [
+        aws_security_group.worker_http_ingress.id,
+        aws_security_group.worker_https_ingress.id,
+      ]
+    },
+
     {
       name                 = "windows-worker-group"
       instance_type        = "m5.large"
