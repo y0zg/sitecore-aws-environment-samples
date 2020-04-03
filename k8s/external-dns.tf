f4d4bfb0dc42fbdb3cca26d3961cf0e2b8f4a9e4
Anders Christian Sørensen <asore@nuuday.dk>
2020-04-03 14:23:04 +0200

    k8s: Comments on ExternalDNS Helm deployment

--- k8s/external-dns.tf
+++ k8s/external-dns.tf
@@ -85,16 +85,39 @@ resource "helm_release" "external_dns" {
     value = data.aws_region.current.name
   }
 
+  # Prevents "ALIAS" A records from being created.
+  # They're specific to AWS and doesn't comply with the DNS RFC.
+  set {
+    name  = "aws.preferCNAME"
+    value = "true"
+  }
+
+  # ExternalDNS creates TXT records with the same name as the CNAME
+  # to keep track of which records ExternalDNS "owns".
+  # TXT records need to be prefixed with something to avoid colissions.
+  # This sets the prefix :-)
+  set {
+    name  = "txtPrefix"
+    value = "foo"
+  }
+
+  # By default, both Ingress and Service objects are scanned to find out
+  # which hostnames to create DNS records for.
+  # This forces ExternalDNS to only scan Ingress objects.
   set {
     name  = "sources.0"
     value = "ingress"
   }
 
+  # Make ExternalDNS's service account assume the given IAM Role.
+  # This is achieved through "IAM Roles for Service Accounts" (IRSA).
   set_string {
     name  = "rbac.serviceAccountAnnotations.eks\\.amazonaws\\.com/role-arn"
     value = aws_iam_role.external_dns.arn
   }
 
+  # Ensure ExternalDNS pods only run on Linux nodes,
+  # in case we have Windows nodes in our cluster too.
   set_string {
     name  = "nodeSelector.kubernetes\\.io/os"
     value = "linux"
